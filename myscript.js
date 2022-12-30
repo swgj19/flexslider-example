7149ad859e3e2fea9e2fd89d7939b95f6c8b0783
Stephen W <swgj19@users.noreply.github.com>
2022-12-30 16:52:16 -0700

    Initial commit

---
+++ myscript.js
@@ -0,0 +1,10 @@
+// Can also be used with $(window).load(function()
+$(document).ready(function () {
+  $(".flexslider").flexslider({
+    animation: "slide",
+    slideShowSpeed: 2000,
+    direction: "vertical",
+    reverse: true,
+    pauseOnHover: true
+  });
+});
